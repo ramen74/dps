92c9e17dea435ccf2ccfdc6105a22bc624349e40
Derek Szydlowski <ramen74@gmail.com>
2012-09-19 08:18:20 +0900

    New project folders

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dps::Application.config.secret_token = '928e8e6d436cfbd5a66ce7c5d7056e283bf8dbc4365dbe80baed703c6fbcd7f02443f7d7fb222c7b078551c36a65960003d2317cab2128410f5b2f0a254eb19e'
