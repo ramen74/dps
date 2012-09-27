9a1cb199f1084448f288387d5154e75a5eefdaa4
Derek Szydlowski <ramen74@gmail.com>
2012-09-27 16:09:05 +0900

    Some early start-up stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Dps::Application.routes.draw do
+  get "main/index"
+
+  get "home/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +53,8 @@ Dps::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+  root :to => "main#index"
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
